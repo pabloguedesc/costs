c0b08f21b2f08b6eaad251fbbffe5ddb7a6cde9f
Pablo <pabloguedesc@gmail.com>
2021-11-02 20:02:58 -0400

    updated

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,19 @@
 import { Link } from "react-router-dom"
-import logo from "../../images/earth.png"
+import logo from "../../images/costs_logo.png"
 import styles from "./Navbar.module.css"
-import { IoHome } from "react-icons/io5";
+// import { IoHome } from "react-icons/io5";
 
 function Navbar() {
     return (
         <nav className={styles.navbar}>
             <Link className={styles.logo} to="/">
-                <img src={logo} alt="Logo" />
+                <img className={styles.logoImg} src={logo} alt="Logo" />
             </Link>
             <div className={styles.menu}>
-                <Link className={styles.link} to="/"> <IoHome className={styles.icon}/> Home</Link>
+                <Link className={styles.link} to="/">Home</Link>
+                <Link className={styles.link} to="/newproject">Projetos</Link>
                 <Link className={styles.link} to="/contact">Contato</Link>
                 <Link className={styles.link} to="/company">Empresa</Link>
-                <Link className={styles.link} to="/newproject">Novo Projeto</Link>
             </div>
         </nav>
     )
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
-
+import img from "../../images/img.png"
+import styles from "./Home.module.css"
 function Home() {
 
     return (
-        <div>
-            <h1>Homekldjnvdsv</h1>
-        </div>
+        <main className={styles.main}>
+            <div className={styles.divMain}>
+                <div>
+                    <h1 className={styles.title}>Seja bem vindo!</h1>
+                    <h1 className={styles.title} id={styles.titleMain}>ao Costs</h1>
+                    <p className={styles.subTitle}>Comece a gerenciar seu dinheiro agora mesmo</p>
+                    <button className={styles.btnStart}>Vamos Começar</button>
+                </div>
+                <img src={img} alt="lal" />
+            </div>
+        </main>
     )
 }
 export default Home
